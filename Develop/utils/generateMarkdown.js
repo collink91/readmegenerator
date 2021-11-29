8758647e4345239ad0b918f8ba1d88f84ca1dd7e
Collin Kerr <collin.kerr@gmail.com>
2021-11-29 16:56:47 -0700

    updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,58 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const inquirer = require('inquirer');
-const fs = require('fs');
-function renderLicenseBadge(license) {}
 
+function renderLicenseBadge(license) {}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
+function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
+
 function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
+function generateMarkdown(response) {
+  //Put down all of the readme info. Pass in responses from inquirer (user response)
+  return `# ${response.title}
+## Description
+${response.description}
+
+## Table of Contents
+If your README is long, add a table of contents to make it easy for users to find what they need.
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+${response.installation}
+
+## Usage
+${response.usage}
+    md
+    ![alt text](assets/images/screenshot.png)
+
+## Contribution Guidelines
+${response.contribution}
+
+## Test Instructions
+${response.test}
+
+## License
+${response.license}
+
+## How to Contribute
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you’d prefer.
+
+## Tests
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+
+## Questions?
+If you have any questions regarding this project, you can reach me at:
+${response.github} or ${response.email}
 `;
 }
-
 module.exports = generateMarkdown;
